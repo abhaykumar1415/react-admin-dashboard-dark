f98f8a519d5db73b6e528347c655850bdcaa2b30
Papat Mayuri <mayuri.papat@qed42.com>
2019-05-13 14:36:27 +0530

    completed with button sizes

--- src/components/buttonlayoutcontainer/index.js
+++ src/components/buttonlayoutcontainer/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './style.css';
 import ButtonWrapper from '../buttonwrapper';
+// import ButtonSize from '../button-sizes';
 export default class ButtonLayoutContainer extends Component {
   constructor(props) {
     super(props);
@@ -18,8 +19,66 @@ export default class ButtonLayoutContainer extends Component {
         {
           title: "Rounded-Filled",
         },
+      ],
+      outline_content_data:
+        [
+          {
+            title: 'Primary',
+            color: '#6387C5'
+          },
+          {
+            title: 'white',
+            color: '#D8D8D8'
+          },
+          {
+            title: 'Accent',
+            color: '#D6BD84'
+          },
+          {
+            title: 'Success',
+            color: '#65AC80'
+          },
+          {
+            title: 'Danger',
+            color: '#ED4740'
+          },
+          {
+            title: 'Dark',
+            color: '#1D242F'
+          },
+          {
+            title: 'Disabled',
+            color: '#D8D8D8'
+          },
+        ],
+      button_sizes: [
+        {
+          title: "XS"
+        },
+        {
+          title: "small"
+        },
+        {
+          title: "default"
+        },
+        {
+          title: "large"
+        }
+      ],
+      button_size_colors: [
+        {
+          backgroundColor: "#005FEA",
+          text_color: '#DAFDFE'
+        },
+        {
+          backgroundColor: "FFFFFF",
+          text_color: '#17171D'
+        },
+        {
+          backgroundColor: "#D8D8D8",
+          text_color: '#616162'
+        }
       ]
-
     }
   }
   render() {
@@ -39,11 +98,45 @@ export default class ButtonLayoutContainer extends Component {
             return (
               <ButtonWrapper
                 key={index}
+                type="button_styles"
                 title={item.title}
+                data={this.state.outline_content_data}
               />
             )
           })
         }
+
+        <div>
+          <div className="color-white button-style font-size-small font-weight margin-bottom">Button Sizes</div>
+          <div className="color-white button_with_sizes">
+            <div className="button-size-header font-size-small font-weight margin-right ">Extra Small</div>
+            <div className="button-size-header font-size-small font-weight margin-right">Small</div>
+            <div className="button-size-header font-size-small font-weight margin-right">Default</div>
+            <div className="button-size-header font-size-small font-weight margin-right">Large</div>
+          </div>
+          <ButtonWrapper
+            button_size_data={this.state.button_sizes}
+            button_size_color={this.state.button_size_colors[0]}
+            type="button_sizes"
+          />
+
+          <ButtonWrapper
+            button_size_data={this.state.button_sizes}
+            button_size_color={this.state.button_size_colors[1]}
+            type="button_sizes"
+          />
+
+          <ButtonWrapper
+            button_size_data={this.state.button_sizes}
+            button_size_color={this.state.button_size_colors[2]}
+            type="button_sizes"
+          />
+        </div>
+        {/* <div>
+          <div className="color-white button-style font-size-small font-weight margin-bottom">Button Dropdowns</div>
+
+        </div> */}
+
       </div>
     )
   }
--- src/components/buttonwrapper/index.js
+++ src/components/buttonwrapper/index.js
@@ -2,62 +2,40 @@ import React, { Component } from 'react'
 import './style.css';
 import Button from '../button';
 export default class ButtonWrapper extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      buttontype: "",
-      outline_content_data:
-        [
-          {
-            title: 'Primary',
-            color: '#6387C5'
-          },
-          {
-            title: 'white',
-            color: '#D8D8D8'
-          },
-          {
-            title: 'Accent',
-            color: '#D6BD84'
-          },
-          {
-            title: 'Success',
-            color: '#65AC80'
-          },
-          {
-            title: 'Danger',
-            color: '#ED4740'
-          },
-          {
-            title: 'Dark',
-            color: '#1D242F'
-          },
-          {
-            title: 'Disabled',
-            color: '#D8D8D8'
-          },
-        ]
-    }
-  }
-
-
-
 
   render() {
+    //console.log("one comp. data :" + this.props.data + " title :" + this.props.title)
     return (
       <div className="button-style-wrapper color-white">
-        <div className="font-size-small font-weight margin-bottom color-black ">{this.props.title}</div>
+        <div className="font-size-small font-weight margin-bottom  ">{this.props.title}</div>
         <div className="buttons-wrapper">
           {
-            this.state.outline_content_data.map((item, index) => {
-              return (
-                <Button key={index}
-                  title={item.title}
-                  color={item.color}
-                  buttontype={this.props.title}
-                />
-              )
-            })
+            this.props.type === 'button_styles' ?
+              this.props.data.map((item, index) => {
+                return (
+                  <Button key={index}
+                    title={item.title}
+                    color={item.color}
+                    buttontype={this.props.title}
+                  />
+                )
+              }) : null
+          }
+        </div>
+        <div className="button_sizes_wrapper">
+          {
+            this.props.type === 'button_sizes' ?
+              this.props.button_size_data.map((item, index) => {
+                return (
+                  <Button
+                    key={index}
+                    title={item.title}
+                    btn_color={this.props.button_size_color}
+                    buttontype={item.title}
+                  />
+                )
+              })
+              : null
           }
         </div>
       </div>
